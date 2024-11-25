707e7a15e3f3c12149814a377967b6a7804cbef9
doodleman218 <dcwolff218@gmail.com>
2024-11-25 12:00:06 -0600

    setting up basic container css and column layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
-}
+  return (
+    <div className="container">
+      <div className="column">
+        <h2>Meet the Team</h2>
+      </div>
+      <div className="column">
+        <h2>View Our Properties</h2>
+      </div>
+      <div className="column">
+        <h2>Social Media Feed</h2>
+      </div>
+    </div>
+  );
+};
 
 export default App;
